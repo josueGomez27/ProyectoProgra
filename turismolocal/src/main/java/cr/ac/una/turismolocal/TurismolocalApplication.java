511ca323e5cec5b0adb588b8af858f89a3753c97
josueGomez27 <138445500+josueGomez27@users.noreply.github.com>
2026-05-31 17:55:23 -0600

    primer commit

---
+++ turismolocal/src/main/java/cr/ac/una/turismolocal/TurismolocalApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.una.turismolocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurismolocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurismolocalApplication.class, args);
+	}
+
+}
---
+++ turismolocal/src/test/java/cr/ac/una/turismolocal/TurismolocalApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.una.turismolocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurismolocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
