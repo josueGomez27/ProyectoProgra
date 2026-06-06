0ef78d9848c7c9360642b65fddbac4966c217cce
josueGomez27 <138445500+josueGomez27@users.noreply.github.com>
2026-06-06 15:52:17 -0600

    PRUEBA FALLIDA , PRUEBA

--- turismo-local-frontend/src/App.test.jsx
+++ turismo-local-frontend/src/App.test.jsx
@@ -3,7 +3,7 @@ import { describe, test, expect } from "vitest";
 import App from "./App";
 
 describe("App", () => {
-  test("renderiza la aplicación sin errores", () => {
+ // test("renderiza la aplicación sin errores", () => {
     render(<App />);
     expect(document.body).toBeTruthy();
   });
