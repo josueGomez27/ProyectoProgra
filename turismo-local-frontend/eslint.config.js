85d0c204af74eb5bcfa96768bea5f636c3c7fc4f
tammy <tammyac27@gmail.com>
2026-06-11 20:47:42 -0600

    responsive navbar

--- turismo-local-frontend/eslint.config.js
+++ turismo-local-frontend/eslint.config.js
@@ -1,21 +1,45 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+    globalIgnores(["dist", "node_modules"]),
+
+    {
+        files: ["**/*.{js,jsx}"],
+
+        extends: [
+            js.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+        ],
+
+        languageOptions: {
+            globals: globals.browser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+
+        rules: {
+            "no-unused-vars": [
+                "warn",
+                {
+                    varsIgnorePattern: "^[A-Z_]",
+                    argsIgnorePattern: "^_",
+                },
+            ],
+
+            "react-refresh/only-export-components": "warn",
+
+            "react-hooks/exhaustive-deps": "off",
+            "react-hooks/immutability": "off",
+            "react-hooks/set-state-in-effect": "off",
+            "react-hooks/rules-of-hooks": "warn",
+        },
     },
-  },
-])
+]);
